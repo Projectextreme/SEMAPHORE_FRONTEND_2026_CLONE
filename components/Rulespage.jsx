f866ea627cb88e7278ec16b3602b0180a76e023f
Thushar R Poojary <thusharbirwazz@gmail.com>
2026-08-28 09:52:06 +0530

    Fix import path for Footer component in Rulespage

--- components/Rulespage.jsx
+++ components/Rulespage.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Footer from "../../components/Footer";
+import Footer from "./Footer";
 
 export default function RulePage() {
   const [rules, setRules] = useState([]);
